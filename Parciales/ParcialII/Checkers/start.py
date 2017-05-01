707945917d77b3be3402f8026378408905cecfe4
Hector F. Jimenez <hfjimenez@utp.edu.co>
2017-04-30 21:45:08 -0500

    main file

--- Parciales/ParcialII/Checkers/start.py
+++ Parciales/ParcialII/Checkers/start.py
@@ -1,1 +1,33 @@
-#Main, Checkers
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#File Details:Start.py, main 
+#ParcialII, Computer Programming IV
+#feedback:hfjimenez@utp.edu.co,dcquiroz@utp.edu.co
+#Features::
+#   Mouse selections 
+#   Per token show the list of Sugestions  
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; Applies version 2 of the License.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from src.graphics import *
+from src.models import *
+from src.views import *
+from src.controller import *
+
+
+def main():
+    print('Is Working the import')
+    #call the controler
+    #it is in charge of making and understanding the logic 
+
+
+if __name__ == '__main__':
+    main()
